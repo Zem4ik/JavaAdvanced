510d840482834321017d01812c06a3b8bd541d75
Zem4ik <vladbullzv@gmail.com>
2017-04-03 17:43:13 +0300

    hw 7 updates

--- src/ru/ifmo/ctddev/Zemtsov/crawler/WebCrawler.java
+++ src/ru/ifmo/ctddev/Zemtsov/crawler/WebCrawler.java
@@ -1,6 +1,8 @@
 package ru.ifmo.ctddev.Zemtsov.crawler;
 
 
+import com.sun.org.apache.regexp.internal.RE;
+
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
@@ -14,12 +16,38 @@ public class WebCrawler implements Crawler {
     private final ExecutorService downloadService;
     private final ExecutorService extractService;
 
+    public static void main(String[] args) {
+        if (args != null && args[0] != null) {
+            int downloads = 10;
+            int extractors = 10;
+            int perHost = 10;
+            if (args[1] != null) downloads = Integer.parseInt(args[1]);
+            if (args[2] != null) extractors= Integer.parseInt(args[2]);
+            if (args[3] != null) perHost = Integer.parseInt(args[3]);
+            try {
+                WebCrawler webCrawler = new WebCrawler(new CachingDownloader(), downloads, extractors, perHost);
+                Result result = webCrawler.download(args[0], 3);
+                System.out.println("downloaded sites: " + result.getDownloaded().size());
+                result.getDownloaded().forEach(System.out::println);
+                System.out.println("number of errors: " + result.getErrors().size());
+                for (Map.Entry pair : result.getErrors().entrySet()) {
+                    System.out.println(pair.getKey() + " ## " + pair.getValue());
+                }
+                webCrawler.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Usage: WebCrawler url [downloads [extractors [perHost]]]");
+        }
+    }
+
     private class LocalInfo {
         ConcurrentHashMap<String, Boolean> URLMap;
         ConcurrentHashMap<String, Integer> downloadingPerHost;
 
         LocalInfo() {
-            URLMap = new ConcurrentHashMap<>();
+            URLMap = new ConcurrentHashMap<String, Boolean>();
             downloadingPerHost = new ConcurrentHashMap<>();
         }
     }
@@ -42,7 +70,7 @@ public class WebCrawler implements Crawler {
             Future<Result> future = futures.poll();
             try {
                 Result result = future.get();
-                if(result.getDownloaded() == null) continue;
+                if (result.getDownloaded() == null) continue;
                 downloaded.addAll(result.getDownloaded());
                 errors.putAll(result.getErrors());
             } catch (InterruptedException | ExecutionException e) {
@@ -57,44 +85,73 @@ public class WebCrawler implements Crawler {
             List<String> downloaded = new ArrayList<>();
             Map<String, IOException> errors = new HashMap<>();
             String host = URLUtils.getHost(url);
-            if (localInfo.downloadingPerHost.get(host) != null && localInfo.downloadingPerHost.get(host) >= perHost) {
-                Future future = downloadService.submit(createDownloadingCallable(futures, url, depth - 1, localInfo));
-                futures.put(future);
-            } else {
-                if (localInfo.URLMap.containsKey(url)) {
-                    return new Result(null, null);
+            final boolean[] flag = {false};
+
+            //flag for containing this url in map
+            localInfo.URLMap.compute(url, (key, value) -> {
+                if (value == null) {
+                    flag[0] = true;
                 }
-                localInfo.downloadingPerHost.compute(host, (key, value) -> (value == null) ? 1 : value + 1);
-                try {
-                    Document document = downloader.download(url);
-                    downloaded.add(url);
-                    localInfo.downloadingPerHost.compute(host, (key, value) -> (value == null) ? 0 : value - 1);
-                    Callable<Result> extractCallable = () -> {
+                return true;
+            });
+
+            //if this url was already downloaded we finish our work
+            if (!flag[0]) return new Result(null, null);
+
+            //waiting for the moment, when we can continue our work
+            flag[0] = false;
+            while (!flag[0]) {
+                localInfo.downloadingPerHost.compute(host, (key, value) -> {
+                    if (value == null) {
+                        flag[0] = true;
+                        return 1;
+                    }
+                    if (value >= perHost) {
+                        Future<Result> future = downloadService.submit(createDownloadingCallable(futures, url, depth - 1, localInfo));
                         try {
-                            if (depth > 1) {
-                                List<String> links = document.extractLinks();
-                                for (String string : links) {
-                                    Future future = downloadService.submit(createDownloadingCallable(futures, string, depth - 1, localInfo));
-                                    futures.put(future);
-                                }
-                            }
-                        } catch (IOException e) {
-                            errors.compute(url, (key, value) -> e);
+                            futures.put(future);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
-                        return new Result(null, null);
-                    };
-                    Future extractFuture = extractService.submit(extractCallable);
-                    futures.put(extractFuture);
-                } catch (IOException e) {
-                    errors.compute(url, (key, value) -> e);
-                }
+                        flag[0] = false;
+                        return value;
+                    }
+                    flag[0] = true;
+                    return value + 1;
+                });
+                if (!flag[0]) Thread.sleep(2 * 1000);
+            }
+
+            //from this moment we now that we can work with this site
+            try {
+                Document document = downloader.download(url);
+                downloaded.add(url);
+                localInfo.downloadingPerHost.compute(host, (key, value) -> (value == null) ? 0 : value - 1);
+                Callable<Result> extractCallable = () -> {
+                    try {
+                        if (depth > 1) {
+                            List<String> links = document.extractLinks();
+                            for (String string : links) {
+                                Future<Result> future = downloadService.submit(createDownloadingCallable(futures, string, depth - 1, localInfo));
+                                futures.put(future);
+                            }
+                        }
+                    } catch (IOException e) {
+                        errors.compute(url, (key, value) -> e);
+                    }
+                    return new Result(null, null);
+                };
+                Future<Result> extractFuture = extractService.submit(extractCallable);
+                futures.put(extractFuture);
+            } catch (IOException e) {
+                errors.compute(url, (key, value) -> e);
             }
             return new Result(downloaded, errors);
         };
     }
 
     @Override
-    public void close()  {
+    public void close() {
         downloadService.shutdownNow();
         extractService.shutdownNow();
     }
--- src/test.java
+++ src/test.java
@@ -1,6 +1,8 @@
 
+import javafx.util.Pair;
 import ru.ifmo.ctddev.Zemtsov.crawler.*;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by vlad on 19.03.17.
@@ -8,9 +10,14 @@ import java.io.IOException;
 public class test {
 
     public static void main(String[] args) throws IOException {
-        WebCrawler webCrawler = new WebCrawler(new CachingDownloader(), 10, 10, 2);
+        WebCrawler webCrawler = new WebCrawler(new CachingDownloader(), 10, 10, 10);
         Result result = webCrawler.download("http://www.kgeorgiy.info/", 2);
-        System.out.println(result.getDownloaded());
+        System.out.println(result.getDownloaded().size());
+        result.getDownloaded().forEach(System.out::println);
+        System.out.println(result.getErrors().size());
+        for (Map.Entry pair : result.getErrors().entrySet()) {
+            System.out.println(pair.getKey() + " ## " + pair.getValue());
+        }
         webCrawler.close();
     }
 }
